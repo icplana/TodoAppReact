e9fd308786026180e1fb95d3fa047128380397ba
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-04-24 18:13:34 +0200

    final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,14 @@
 export const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-red-100 to-red-300 font-bold absolute bottom-0 px-9 py-5 w-full flex justify-between ">
-
-        Pequeña aplicación para practicas. Hecha con React y TailwindCSS
-        <br />
+        <div>
+            Pequeña aplicación para practicas. Hecha con React y TailwindCSS
+            <br />
+            <a href="https://github.com/icplana/TodoAppReact"><p className=" mt-3 underline animate-bounce">Github Repo</p></a>
+        </div>
         <div>
             Ignasi
-            <a href="https://github.com/icplana" target="_blank" rel="noreferrer"><img src="../src/assets/github.svg" alt="" width='30px' /></a>
+            <a  href="https://github.com/icplana" target="_blank" rel="noreferrer"><img  className="animate-bounce mt-3" src="../src/assets/github.svg" alt="" width='30px' /></a>
         </div>
     </footer>
   )
